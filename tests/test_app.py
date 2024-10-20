bea028424ddc7b4729fbdbef3fb8ba13dd4e9c85
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2024-10-20 16:42:54 +0200

    fix: cicd app tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,28 +6,28 @@ from playwright.sync_api import Page, expect
 def test_reject_improper_login_attempts(page: Page) -> None:
     page.goto("http://localhost:8501/")
 
-    page.get_by_test_id("baseButton-secondaryFormSubmit").click()
-    expect(
-        page.get_by_test_id("stNotificationContentError").get_by_role("paragraph")
-    ).to_contain_text("Login failed")
+    page.get_by_test_id("stBaseButton-secondaryFormSubmit").click()
+    expect(page.get_by_test_id("stAlert").get_by_role("paragraph")).to_contain_text(
+        "Login failed"
+    )
 
     page.get_by_label("username").click()
     page.get_by_label("username").fill("user")
-    page.get_by_test_id("baseButton-secondaryFormSubmit").click()
-    expect(
-        page.get_by_test_id("stNotificationContentError").get_by_role("paragraph")
-    ).to_contain_text("Login failed")
+    page.get_by_test_id("stBaseButton-secondaryFormSubmit").click()
+    expect(page.get_by_test_id("stAlert").get_by_role("paragraph")).to_contain_text(
+        "Login failed"
+    )
 
     page.get_by_label("password", exact=True).click()
     page.get_by_label("password", exact=True).fill("dummy")
-    page.get_by_test_id("baseButton-secondaryFormSubmit").click()
-    expect(
-        page.get_by_test_id("stNotificationContentError").get_by_role("paragraph")
-    ).to_contain_text("Login failed")
+    page.get_by_test_id("stBaseButton-secondaryFormSubmit").click()
+    expect(page.get_by_test_id("stAlert").get_by_role("paragraph")).to_contain_text(
+        "Login failed"
+    )
 
     page.get_by_label("username").click()
     page.get_by_label("username").fill("")
-    page.get_by_test_id("baseButton-secondaryFormSubmit").click()
-    expect(
-        page.get_by_test_id("stNotificationContentError").get_by_role("paragraph")
-    ).to_contain_text("Login failed")
+    page.get_by_test_id("stBaseButton-secondaryFormSubmit").click()
+    expect(page.get_by_test_id("stAlert").get_by_role("paragraph")).to_contain_text(
+        "Login failed"
+    )
